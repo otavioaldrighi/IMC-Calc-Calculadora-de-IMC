96c66496cb8f72a21f629d6363d1182230d68c3e
Otávio Martins Aldrighi <94414122+otavioaldrighi@users.noreply.github.com>
2022-07-27 21:36:17 -0300

    IMC Calc Completo (JS Simples)

---
+++ imc.js
@@ -0,0 +1,40 @@
+
+const calcular = document.getElementById('calcular');
+
+
+function imc(){
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if(nome.value !== '' && altura !== '' && peso !== '') {
+        
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = '';
+
+        if(valorIMC <18.5){
+            classificacao = 'abaixo do peso. ';
+        }else if(valorIMC < 25){
+            classificacao = 'com peso ideal. Parabéns! 😃';
+        }else if(valorIMC < 30){
+            classificacao = 'levemente acima do peso';
+        }else if(valorIMC < 35){
+            classificacao = 'com obesidade grau I';
+        }else if (valorIMC < 40) {
+            classificacao = 'com obesidade grau II';
+        }else {
+            classificacao = 'com obesidade grau III'
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+        
+    }else {
+        resultado.textContent = '💪Preencha todos os campos ! 💪'
+    }
+
+}
+
+calcular.addEventListener('click', imc);
